7b779599817ffe60934c5523a53a3f9fd173fac2
Kyle-the-coder <kylethacoder@gmail.com>
2025-04-22 13:05:04 -0600

    updated

--- client/src/pages/Landing/Landing.jsx
+++ client/src/pages/Landing/Landing.jsx
@@ -4,9 +4,12 @@ import { Hero } from "../../sections/LPSections/Hero/Hero";
 function LandingPage() {
   return (
     <div className="main-page">
+      {/* Section: */}
       <Hero />
+      {/* Component: */}
       <BlogsContainer title="Latest" isVertical={false} />
       <BlogsContainer title="Trending" isVertical={false} />
+      {/* Section: */}
     </div>
   );
 }
