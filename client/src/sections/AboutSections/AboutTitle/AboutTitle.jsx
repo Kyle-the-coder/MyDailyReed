aca94edf5a6c6f3e46d7680b5130746910039644
Kyle-the-coder <kylethacoder@gmail.com>
2025-04-23 10:43:01 -0600

    updated

--- client/src/sections/AboutSections/AboutTitle/AboutTitle.jsx
+++ client/src/sections/AboutSections/AboutTitle/AboutTitle.jsx
@@ -3,7 +3,7 @@ import aboutTree from "../../../assets/placeholders/MDRAboutTree.png";
 export function AboutTitle() {
   return (
     <section className="about-title-main-container charcoal-bg">
-      <h1 className="outfit-font ">My Daily Reed</h1>
+      <h1 className="playfair-font ">My Daily Reed</h1>
       <img src={aboutTree} />
       <h1 className="playfair-font">About Me</h1>
     </section>
