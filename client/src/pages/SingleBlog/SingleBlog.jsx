764410b3910cfc5cde969c62fbb683ca2d72805f
Kyle-the-coder <kylethacoder@gmail.com>
2025-04-25 11:53:26 -0600

    updated

--- client/src/pages/SingleBlog/SingleBlog.jsx
+++ client/src/pages/SingleBlog/SingleBlog.jsx
@@ -3,12 +3,14 @@ import { SbTitle } from "../../sections/SingleBlogSections/SbTitle/SbTitle";
 import { scrollToSection } from "../../components/SmoothScroll";
 import { SbArticle } from "../../sections/SingleBlogSections/SbArticle/SbArticle";
 import { Social } from "../../sections/SingleBlogSections/Social/Social";
+import { Sidebar } from "../../components/Sidebar/Sidebar";
 
 function SingleBlog() {
   return (
     <section id="single-blog" className="display-column">
       <SbTitle />
       <SbArticle />
+      <Sidebar />
       <Social />
     </section>
   );
--- client/src/sections/SingleBlogSections/Social/Social.jsx
+++ client/src/sections/SingleBlogSections/Social/Social.jsx
@@ -1,17 +1,23 @@
+import { useState } from "react";
 import likeEmpty from "../../../assets/icons/likeEmpty.png";
+import likeFilled from "../../../assets/icons/likeFilled.png";
 import { WordButton } from "../../../components/Buttons/WordButton/WordButton";
 
 import "./social.css";
 
 export function Social() {
+  const [isLike, setIsLike] = useState(false);
   return (
     <section className="social-main-container">
-      <div className="flex-between">
+      <div className="like-and-comment-container">
         <div className="like-and-share">
           <div className="like-container">
             <div className="like">
-              <img src={likeEmpty} />
-              <p>100 likes</p>
+              <img
+                src={isLike ? likeFilled : likeEmpty}
+                onClick={() => setIsLike(!isLike)}
+              />
+              <p>{isLike ? "101 likes" : "100 likes"}</p>
             </div>
             <h1 className="outfit-font">Leave a like</h1>
           </div>
@@ -36,7 +42,13 @@ export function Social() {
         </div>
       </div>
 
-      <div className="comment-container"></div>
+      <div className="comment-container">
+        <h1 className="outfit-font">Comments:</h1>
+        <div className="comments silver-bg">
+          <h1 className="commenter-name outfit-font">Reader1:</h1>
+          <p className="comment outfit-font">I love this article</p>
+        </div>
+      </div>
     </section>
   );
 }
