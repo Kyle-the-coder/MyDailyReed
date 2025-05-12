9abda324150050c8949d050b0d20afe3732fd453
Kyle-the-coder <kylethacoder@gmail.com>
2025-05-12 13:23:00 -0600

    updated

--- server/netlify/functions/server.js
+++ server/netlify/functions/server.js
@@ -2,6 +2,8 @@ const serverless = require("serverless-http");
 const connectDB = require("../../config/mongoose.config");
 const app = require("../../app");
 
+console.log("Netlify function cold start", new Date().toISOString());
+
 module.exports.handler = serverless(async (req, res) => {
   await connectDB();
   return app(req, res);
