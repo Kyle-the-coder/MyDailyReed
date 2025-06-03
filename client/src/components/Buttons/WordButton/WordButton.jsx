23c7c0b84f2aea48a8c47677ea3052a94e6c2024
Kyle-the-coder <kylethacoder@gmail.com>
2025-06-03 10:58:33 -0600

    updated

--- client/src/components/Buttons/WordButton/WordButton.jsx
+++ client/src/components/Buttons/WordButton/WordButton.jsx
@@ -1,16 +1,27 @@
+import { useState } from "react";
 import "./wordbutton.css";
 
 export function WordButton({ text, fontSize, margin, onClick }) {
+  const [isHover, setIsHover] = useState(false);
   return (
     <button
-      className="word-button green-bg outfit-font"
+      className="button-main green-bg outfit-font"
+      onMouseEnter={() => setIsHover(true)}
+      onMouseLeave={() => setIsHover(false)}
       style={{
         fontSize: fontSize ? fontSize : "clamp(1.8rem, 1.9vw, 2rem)",
         margin: margin,
       }}
       onClick={onClick}
     >
-      {text}
+      <p
+        style={{
+          zIndex: 2,
+        }}
+      >
+        {text}
+      </p>
+      <div className={`hover-bg ${isHover ? "active" : ""}`}></div>
     </button>
   );
 }
